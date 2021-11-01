f57dfd4ffed39af136da7c5ae3132403aaa133e3
JoFox <jingran.yu1988@gmail.com>
2021-11-01 17:49:36 +1100

    Update log level and version bump from 1.0.0 to 1.0.1 Signed-off-by: JoFox <jingran.yu1988@gmail.com>

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 import Holidays from 'date-holidays'
+import log from 'loglevel'
 
-export function isPublicHoliday() {
-    var d = new Date()
+// Input local date and time with timezone information to get the right results.
+// Format example "2021-10-03 00:00:00 GMT+1100" - Use this to check if Sydney time 2021-10-03 is a public holiday.
+// Leave input undefined to check if right now is a public holiday.
+
+export function isPublicHoliday(date) {
+    if (date === undefined) {
+        var d = new Date()
+        log.debug(d)
+    }
+    else {
+        var d = new Date(date)
+        log.debug(d)
+    }
     var hd = new Holidays('AU', 'NSW')
     var thisYear = d.getFullYear()
     var thisYearHoliday = hd.getHolidays(thisYear)
-    // console.log(thisYearHoliday)
+    log.debug(thisYearHoliday)
     thisYearHoliday.forEach(element => {
         if (element.type == 'public') {
             var nowTime = d.getTime()
             var startTime = element.start.getTime()
             var endTime = element.end.getTime()
             if (startTime <= nowTime && nowTime <= endTime) {
-                console.log("It's public holiday - " + element.name)
+                log.info("It's public holiday - " + element.name)
                 return true
             }
         } 
         else {
             var w = d.getDay()
             if ( w == 0 || w == 6) {
-                console.log("It's weekends!")
+                log.info("It's weekends!")
                 return true
                 } 
                 else {
-                    console.log("It's working day!")
+                    log.info("It's working day!")
+                    log.info(element)
                     return false
                 }
         }
@@ -31,5 +44,4 @@ export function isPublicHoliday() {
 )}
 
 
-
-// isPublicHoliday()
+isPublicHoliday("2021-10-04 00:00:00 GMT+1100")

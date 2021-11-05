51249918edc721030334cfffedad886a8a8ee598
JoFox <jingran.yu1988@gmail.com>
2021-11-05 14:07:29 +1100

    Updated index.ts and unit test passed Signed-off-by: JoFox <jingran.yu1988@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # npm-package-practice
 
-This is for test only!
+This is NSW, AU public holiday checker. You can use this package to check if your local date is a holiday in NSW, AU.
+
+Input format will be like this ```"2021-10-03 00:00:00 GMT+0800"```. If leave input blank, the package will use local right now time.
+
--- index.ts
+++ index.ts
@@ -1,49 +1,96 @@
 import Holidays from 'date-holidays'
-import isHoliday from 'date-holidays'
 import log from 'loglevel'
 
-log.setLevel('debug')
+log.setLevel('warn')
 // Input local date and time with timezone information to get the right results.
-// Format example "2021-10-03 00:00:00 GMT+1100" - Use this to check if Sydney time 2021-10-03 is a public holiday.
-// Leave input undefined to check if right now is a public holiday.
+// Format example "2021-10-03 00:00:00 GMT+0800" - Use this to check if Chinese date 2021-10-03 is a public holiday in NSW, AU.
+// Leave input undefined to check if right now is a public holiday in NSW, AU.
 
-export function isPublicHoliday(date: string) {
+export function convertUTCtoAEDT(date?: string) {
     if (date === undefined) {
-        var d = new Date()
-        log.debug(d)
+        var utcDate = new Date()
+        var SydTime = new Date(utcDate.getTime() + 11*60*60*1000)
+        var SydDate = SydTime.toISOString().split('T')[0]
+        return SydDate
     }
     else {
-        var d = new Date(date)
-        log.debug(d)
+        var utcDate = new Date(date)
+        var SydTime = new Date(utcDate.getTime() + 11*60*60*1000)
+        var SydDate = SydTime.toISOString().split('T')[0]
+        log.debug(SydDate)
+        return SydDate
     }
+}
+
+export function listDateOfHoliday(date?: string) {
+    var thisYear = convertUTCtoAEDT(date).split('-')[0]
     var hd = new Holidays('AU', 'NSW')
-    var thisYear = d.getFullYear()
     var thisYearHoliday = hd.getHolidays(thisYear)
-    log.debug(thisYearHoliday)
-    thisYearHoliday.forEach(element => {
-        if (element.type == 'public') {
-            var nowTime = d.getTime()
-            var startTime = element.start.getTime()
-            var endTime = element.end.getTime()
-            if (startTime <= nowTime && nowTime <= endTime) {
-                log.info("It's public holiday - " + element.name)
-                return true
-            }
-        } 
-        else {
-            var w = d.getDay()
-            if ( w == 0 || w == 6) {
-                log.info("It's weekends!")
-                return true
-                } 
-                else {
-                    log.info("It's working day!")
-                    log.info(element)
-                    return false
-                }
+    var ListOfDate = []
+    var hLen = thisYearHoliday.length
+    for (let i = 0; i < hLen; i++) {
+        if (thisYearHoliday[i].type === 'public') {
+            var shortDate = thisYearHoliday[i].date.split(' ')[0]
+            ListOfDate.push(shortDate)
         }
     }
-)}
+    log.debug(ListOfDate)
+    return ListOfDate
+}
+
+export function isPublicHoliday(date?: string) {
+    var AEDTDate = convertUTCtoAEDT(date)
+    log.debug(AEDTDate)
+    var ShortList = listDateOfHoliday(date)
+    if (ShortList.includes(AEDTDate)) {
+        log.debug("It is public holiday in NSW, AU")
+        return true
+    } else {
+        log.debug("It's NOT public holiday in NSW, AU")
+        return false
+    }
+}
+// Test only!
+// isPublicHoliday("2021-12-31 21:01:00 GMT+1100")
+
+// isPublicHoliday("2021-12-25 00:00:00 GMT+1100")
+// export function isPublicHoliday(date: string) {
+//     if (date === undefined) {
+//         var d = new Date()
+//         log.debug(d)
+//     }
+//     else {
+//         var d = new Date(date)
+//         log.debug(d)
+//     }
+//     var hd = new Holidays('AU', 'NSW')
+//     var thisYear = d.getFullYear()
+//     var thisYearHoliday = hd.getHolidays(thisYear)
+//     log.debug(thisYearHoliday)
+//     thisYearHoliday.forEach(element => {
+//         if (element.type == 'public') {
+//             var nowTime = d.getTime()
+//             var startTime = element.start.getTime()
+//             var endTime = element.end.getTime()
+//             if (startTime <= nowTime && nowTime <= endTime) {
+//                 log.info("It's public holiday - " + element.name)
+//                 return true
+//             }
+//         } 
+//         else {
+//             var w = d.getDay()
+//             if ( w == 0 || w == 6) {
+//                 log.info("It's weekends!")
+//                 return true
+//                 } 
+//                 else {
+//                     log.info("It's working day!")
+//                     log.info(element)
+//                     return false
+//                 }
+//         }
+//     }
+// )}
 
 
-// isPublicHoliday("2021-10-04 00:00:00 GMT+1100")
+
